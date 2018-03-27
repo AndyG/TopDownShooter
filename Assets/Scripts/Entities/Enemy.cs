c9ff742bf851201ac98b3a64b794d77b640fd9d0
andygarron <anderson.garron@gmail.com>
2018-03-26 19:33:19 -0700

    spawners

---
+++ Assets/Scripts/Entities/Enemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+  public GameObject target;
+  public float speed;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (target != null)
+    {
+      float step = speed * Time.deltaTime;
+      transform.position = Vector3.MoveTowards(transform.position, target.transform.position, step);
+    }
+  }
+}
---
+++ Assets/Scripts/Utils/Spawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+  public GameObject spawnObject;
+
+  public GameObject target;
+  public float interval;
+  private float timeSince = 0f;
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    timeSince += Time.deltaTime;
+    if (timeSince >= interval)
+    {
+      spawn();
+      timeSince = 0f;
+    }
+  }
+
+  private void spawn()
+  {
+    GameObject tempGo = GameObject.Instantiate(spawnObject, Vector3.zero, Quaternion.identity) as GameObject;
+    Enemy enemyScript = tempGo.GetComponent<Enemy>();
+    tempGo.transform.position = transform.position;
+    enemyScript.target = target;
+  }
+}
