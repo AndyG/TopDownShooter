776e33b159fe5f6973c0cd3ec0778fd8ba584816
andygarron <anderson.garron@gmail.com>
2018-03-31 16:26:05 -0700

    added a bunch of stuff

---
+++ Assets/Bullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+  // Use this for initialization
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  void OnTriggerEnter2D(Collider2D other)
+  {
+    Debug.Log("on trigger enter");
+    Shootable shootable = other.GetComponent<Shootable>();
+    if (shootable != null)
+    {
+      shootable.handleShot(this);
+      Destroy(this.gameObject);
+    }
+  }
+}
