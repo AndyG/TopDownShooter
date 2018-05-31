d8de4f8d9883e229627cda1971d220ce1fd4d08d
andygarron <anderson.garron@gmail.com>
2018-05-31 11:51:56 -1000

    attempting tilesheets, blend trees

---
+++ Assets/SamplePlayer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SamplePlayer : MonoBehaviour
+{
+
+  [SerializeField]
+  float topSpeed = 15;
+
+  Rigidbody2D rb;
+
+  Animator animator;
+
+  // Use this for initialization
+  void Start()
+  {
+    rb = GetComponent<Rigidbody2D>();
+    animator = GetComponent<Animator>();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    float horiz = Input.GetAxis("Horizontal");
+    float vert = Input.GetAxis("Vertical");
+
+    updateAnimator(horiz, vert);
+
+    Vector2 velocity = new Vector2(horiz, vert);
+    rb.AddForce(velocity, ForceMode2D.Impulse);
+    rb.velocity = Vector2.ClampMagnitude(rb.velocity, topSpeed);
+  }
+
+  void updateAnimator(float horiz, float vert)
+  {
+    animator.SetFloat("HorizInput", horiz);
+  }
+}
