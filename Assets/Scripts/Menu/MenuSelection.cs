a3d7710cc22fda02bf7c66af44faaac1c8a89b20
andygarron <anderson.garron@gmail.com>
2018-07-01 14:27:14 -0700

    More MainMenu stuff

---
+++ Assets/Scripts/Menu/MenuSelection.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSelection : MonoBehaviour
+{
+
+  public void OnPlayClicked()
+  {
+    SceneManager.LoadScene("GrassArena1");
+  }
+
+  public void OnQuitClicked()
+  {
+    Application.Quit();
+  }
+}
